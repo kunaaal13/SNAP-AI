5d8e09e77e2a9d26e42ff35008da9efb6b2aec8c
Kunaaal13 <mrkunalyadav7@gmail.com>
2023-01-30 20:28:23 +0530

    Tailwind setup

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '../styles/globals.css'
 
 export default function RootLayout({
   children,
@@ -6,7 +6,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
